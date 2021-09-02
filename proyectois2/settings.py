89006523d766a79670318aef6c1789d53496f1d3
RamosDer <der.ramos93@gmail.com>
2021-09-01 23:42:28 -0400

    tag version1.0

--- proyectois2/settings.py
+++ proyectois2/settings.py
@@ -166,3 +166,6 @@ SOCIALACCOUNT_PROVIDERS = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# static root APACHE
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
